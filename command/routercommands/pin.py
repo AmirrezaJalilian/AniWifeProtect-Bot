6a4a800e3afe3d3c62ddb3fb33b7ef0f23ed4a74
AmirrezaJalilian <TheCleverMr@gmail.com>
2025-12-16 08:58:58 +0330

    fix: pin & unpin command

--- command/routercommands/pin.py
+++ command/routercommands/pin.py
@@ -6,12 +6,12 @@ from log.logger import send_log
 from log.type import LogTypes
 
 
-async def pin(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def pin(update: Update, context: ContextTypes.DEFAULT_TYPE, args):
 
     reply = update.effective_message.reply_to_message
     userid = update.effective_user.id
 
-    if not await is_staff(userid):
+    if not is_staff(userid):
         return
 
     if not reply:
@@ -20,15 +20,19 @@ async def pin(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     message_id = reply.id
     await update.effective_chat.pin_message(message_id)
+    await update.effective_message.reply_text(
+        "Pinned Message",
+        reply_to_message_id=message_id
+    )
     await send_log(context, LogTypes.INFO, "Group", f"New Pin Message: {message_id}")
 
 
-async def unpin(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def unpin(update: Update, context: ContextTypes.DEFAULT_TYPE, args):
 
     reply = update.effective_message.reply_to_message
     userid = update.effective_user.id
 
-    if not await is_staff(userid):
+    if not is_staff(userid):
         return
 
     if not reply:
@@ -36,5 +40,9 @@ async def unpin(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     message_id = reply.id
-    await update.effective_chat.pin_message(message_id)
-    await send_log(update, context, "Group", f"New UnPin Message: {message_id}")
+    await update.effective_chat.unpin_message(message_id)
+    await update.effective_message.reply_text(
+        "UnPinned Message",
+        reply_to_message_id=message_id
+    )
+    await send_log(context, LogTypes.INFO, "Group", f"New UnPin Message: {message_id}")
