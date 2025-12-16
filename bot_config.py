2e8d04ad36e54d557fe20924de4b0884fe2d6ce2
AmirrezaJalilian <TheCleverMr@gmail.com>
2025-12-16 08:34:46 +0330

    fix

--- bot_config.py
+++ bot_config.py
@@ -1,6 +1,6 @@
 import datetime as dt
 
-TOKEN = "8452805938:AAFrnH0mEeQRwcgf4Q2vcVXVJ2CSq_yFoXI"
+TOKEN = "TOKEN"
 LOG_GROUP_ID = -1002578056469
 OWNER_ID = 7965609913
 LINK_REGEX = r"(?:https?://)?(?:t\.me/)([A-Za-z0-9_]+)/?|@([A-Za-z0-9_]+)"
